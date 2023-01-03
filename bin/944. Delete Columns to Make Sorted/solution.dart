f7e70e7337c1b2939ad01048582aa4a10033f8f1
Mahmoud Ashour <mashour365@gmail.com>
2023-01-03 14:31:40 +0200

    Solved daily problem https://leetcode.com/problems/delete-columns-to-make-sorted/

---
+++ bin/944. Delete Columns to Make Sorted/solution.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  print(minDeletionSize(["rrjk", "furt", "guzm"]));
+}
+int minDeletionSize(List<String> strs) {
+  var deletedColumnsCount = 0;
+  for (var i = 0; i < strs[0].length; i++) {
+    for (var j = 0; j < strs.length - 1; j++) {
+      if (strs[j][i].codeUnits[0] > strs[j + 1][i].codeUnits[0]) {
+        deletedColumnsCount++;
+        break;
+      }
+    }
+  }
+  return deletedColumnsCount;
+}
