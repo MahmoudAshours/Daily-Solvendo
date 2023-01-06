6032f34d500086eb04fb0c0001359013ec80cde2
Mahmoud Ashour <mashour365@gmail.com>
2023-01-06 19:37:38 +0200

    Solved leetcode problem https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/

---
+++ bin/1790. Check if One String Swap Can Make Strings Equal/solution.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+	print(areAlmostEqual("abcd", "dcba"))
+}
+
+func areAlmostEqual(s1 string, s2 string) bool {
+	ind, count := -1, 0
+	for i := 0; i < len(s2) && count <= 2; i++ {
+		if s1[i] == s2[i] {
+			continue
+		} else if ind >= 0 && !(s1[ind] == s2[i] && s1[i] == s2[ind]) {
+			return false
+		} else {
+			ind = i
+		}
+		count++
+	}
+	return count == 2 || count == 0
+}
