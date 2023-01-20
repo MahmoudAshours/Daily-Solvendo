17671e110f300da7f4044e507220306aebb86680
Mahmoud Ashour <mashour365@gmail.com>
2023-01-20 16:57:35 +0200

    Solved leetcode daily challenge https://leetcode.com/problems/non-decreasing-subsequences

---
+++ bin/491. Non-decreasing Subsequences/solution.go
@@ -0,0 +1,33 @@
+package main
+
+type void struct{}
+
+var member void
+
+func main() {
+
+	print(findSubsequences([]int{4, 6, 7, 7}))
+
+}
+func findSubsequences(nums []int) (res [][]int) {
+	var dfs func(int, []int)
+	dfs = func(l int, sl []int) {
+		if len(sl) > 1 {
+			cp := make([]int, len(sl))
+			copy(cp, sl)
+			res = append(res, cp)
+		}
+		seen := make(map[int]bool, len(nums)-l)
+		for r := l; r < len(nums); r++ {
+			if (l > 0 && nums[r] < nums[l-1]) || seen[nums[r]] {
+				continue
+			}
+			seen[nums[r]] = true
+			dfs(r+1, append(sl, nums[r]))
+		}
+	}
+
+	dfs(0, []int{})
+
+	return res
+}
