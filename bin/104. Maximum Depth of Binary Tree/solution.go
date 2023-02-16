c90af3cdfd5252e7f4c8658d93a5e5714c475630
Mahmoud Ashour <mashour365@gmail.com>
2023-02-17 00:32:01 +0200

    Solved leetcode daily challenge https://leetcode.com/problems/maximum-depth-of-binary-tree

---
+++ bin/104. Maximum Depth of Binary Tree/solution.go
@@ -0,0 +1,24 @@
+package main
+
+func main(){
+
+}
+ type TreeNode struct {
+      Val int
+      Left *TreeNode
+      Right *TreeNode
+}
+
+ func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
+
+func max(x int, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
