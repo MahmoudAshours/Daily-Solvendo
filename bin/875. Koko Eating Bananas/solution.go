be26db4a1f74d52a0bbbb2e11fcd6a5fa0311d3a
Mahmoud Ashour <mashour365@gmail.com>
2023-03-08 22:00:18 +0200

    Solved leetcode daily problem https://leetcode.com/problems/koko-eating-bananas/description/

---
+++ bin/875. Koko Eating Bananas/solution.go
@@ -0,0 +1,35 @@
+package main
+func main(){
+	print(minEatingSpeed([]int{3,6,7,11},8))
+}
+
+func minEatingSpeed(piles []int, h int) int {
+    left, right := 1, max(piles)
+    
+    for left < right {
+        mid := (left + right) / 2
+        hours := 0
+        
+        for _, pile := range(piles) {
+            hours += int(math.Ceil(float64(pile) / float64(mid)))
+        }
+        
+        if hours <= h {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+    return right
+}
+
+func max(arr []int) int {
+    max := arr[0]
+    
+    for _, val := range(arr) {
+        if max < val {
+            max = val
+        }
+    }
+    return max
+}
