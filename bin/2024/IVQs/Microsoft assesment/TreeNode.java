71785d0a71f27c116fa566a9c3865db4f32be626
Mahmoud Ashour <mashour365@gmail.com>
2024-10-12 17:45:28 +0300

    Create TreeNode.java

---
+++ bin/2024/IVQs/Microsoft assesment/TreeNode.java
@@ -0,0 +1,16 @@
+public class TreeNode {
+    /*
+     * left
+     * right
+     * val
+     * constructor
+     */
+
+    TreeNode left;
+    TreeNode right;
+    int val;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
