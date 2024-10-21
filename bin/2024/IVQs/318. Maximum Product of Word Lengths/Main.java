cc01788c2e74c996274e79a8024edac86e1614b5
Mahmoud Ashour <mashour365@gmail.com>
2024-10-21 18:42:06 +0300

    Create Main.java

---
+++ bin/2024/IVQs/318. Maximum Product of Word Lengths/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.maxProduct(new String[]{"abcw", "baz", "foo", "bar", "xtfn", "abcdef"});
+    }
+}
