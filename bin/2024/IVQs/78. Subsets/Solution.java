ae9ada16ca814ad99a1e461a47a164e7be30ce52
Mahmoud Ashour <mashour365@gmail.com>
2024-10-21 18:43:20 +0300

    Create Solution.java

---
+++ bin/2024/IVQs/78. Subsets/Solution.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+  public List<List<Integer>> subsets(int[] nums){
+    System.out.println(nums[0]);
+    ArrayList a = new ArrayList<>();
+    ArrayList b = new ArrayList<>();
+    a.add(b);
+    return a;
+  }
+}
