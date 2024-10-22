b03a3e8c11ff3a02879590670f1bb05d88c4f226
Mahmoud Ashour <mashour365@gmail.com>
2024-10-22 07:15:11 +0300

    Create Main.java

---
+++ bin/2024/IVQs/394. Decode String/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Integer firstNumber = 10;
+        Integer secondNumber = 10;
+        System.out.println(firstNumber == secondNumber);
+        Integer thirdNumber = 200;
+        Integer fourthNumber = 200;
+        System.out.println(thirdNumber == fourthNumber);
+    }
+}
